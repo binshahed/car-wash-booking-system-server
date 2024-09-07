8c7f86dff92f62fa40dcbb5c16c1be8e34cc23ee
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-09-07 16:13:04 +0600

    update routing

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -5,10 +5,11 @@ import auth from '../../middlewares/auth';
 const router = Router();
 
 router.route('/').get(auth('admin'), userController.getAllUsers);
-router.route('/me').get(auth('admin', 'user'), userController.getMe);
 router
-  .route('/update-profile')
+  .route('/me')
+  .get(auth('admin', 'user'), userController.getMe)
   .patch(auth('admin', 'user'), userController.updateProfile);
+
 router.route('/:id').patch(auth('admin'), userController.updateUserStatus);
 
 export const userRouter = router;
