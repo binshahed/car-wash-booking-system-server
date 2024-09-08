3d29fd1e6aeabd67195e828d679ecd61240be959
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-09-08 19:38:25 +0600

    add payment

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -9,15 +9,15 @@ import mongoose from 'mongoose';
 import NotFoundError from '../../errors/NotFoundError';
 
 const createBooking = async (payloadUser: any, payload: TBookingInput) => {
-  const {
-    serviceId,
-    slotId,
-    manufacturingYear,
-    registrationPlate,
-    vehicleBrand,
-    vehicleModel,
-    vehicleType,
-  } = payload;
+  // const {
+  //   serviceId,
+  //   slotId,
+  //   manufacturingYear,
+  //   registrationPlate,
+  //   vehicleBrand,
+  //   vehicleModel,
+  //   vehicleType,
+  // } = payload;
 
   // find service
   const service = await ServiceModel.findById(payload.serviceId);
@@ -40,53 +40,76 @@ const createBooking = async (payloadUser: any, payload: TBookingInput) => {
   const session = await mongoose.startSession();
   await session.startTransaction();
 
-  try {
-    // create new booking object
-    const newBooking = {
-      customer: payloadUser._id,
-      service: serviceId,
-      slot: slotId,
-      manufacturingYear,
-      registrationPlate,
-      vehicleBrand,
-      vehicleModel,
-      vehicleType,
-    };
+  console.log(payload);
+  console.log('service', service, 'slot', slot);
 
-    // update slot available to booked
-    await SlotModel.findOneAndUpdate(
-      { _id: slotId },
-      { $set: { isBooked: 'booked' } },
-      { session, new: true },
-    );
-    // create new booking
-    const booking = await BookingModel.create([newBooking], { session });
+  const formData = {
+    cus_name: payloadUser?.name,
+    cus_email: payloadUser?.email,
+    cus_phone: payloadUser?.phone,
+    amount: service?.price,
+    tran_id: slot?._id,
+    signature_key: '28c78bb1f45112f5d40b956fe104645100',
+    store_id: 'aamarpay',
+    currency: 'BDT',
+    desc: 'Merchant Registration Payment',
+    cus_add1: '53, Gausul Azam Road, Sector-14, Dhaka, Bangladesh',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_country: 'Bangladesh',
+    // success_url: `${baseUrl}callback`,
+    // fail_url: `${baseUrl}callback`,
+    // cancel_url: `${baseUrl}callback`,
+    type: 'json',
+  };
 
-    // if booking not success
+  // try {
+  //   // create new booking object
+  //   const newBooking = {
+  //     customer: payloadUser._id,
+  //     service: serviceId,
+  //     slot: slotId,
+  //     manufacturingYear,
+  //     registrationPlate,
+  //     vehicleBrand,
+  //     vehicleModel,
+  //     vehicleType,
+  //   };
 
-    if (!booking) {
-      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create booking');
-    }
+  //   // update slot available to booked
+  //   await SlotModel.findOneAndUpdate(
+  //     { _id: slotId },
+  //     { $set: { isBooked: 'booked' } },
+  //     { session, new: true },
+  //   );
+  //   // create new booking
+  //   const booking = await BookingModel.create([newBooking], { session });
 
-    // populate customer, service and slot details
-    const result = await booking[0].populate([
-      { path: 'customer', select: '-role -createdAt -updatedAt' },
-      { path: 'service', select: '-createdAt -updatedAt -__v' },
-      { path: 'slot', select: '-createdAt -updatedAt -__v' },
-    ]);
+  //   // if booking not success
 
-    // end session
-    await session.commitTransaction();
-    await session.endSession();
+  //   if (!booking) {
+  //     throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create booking');
+  //   }
 
-    // return result with populated customer, service and slot details
-    return result;
-  } catch (err: any) {
-    // rollback session in case of error
-    await session.abortTransaction();
-    await session.endSession();
-    throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, err.message);
-  }
+  //   // populate customer, service and slot details
+  //   const result = await booking[0].populate([
+  //     { path: 'customer', select: '-role -createdAt -updatedAt' },
+  //     { path: 'service', select: '-createdAt -updatedAt -__v' },
+  //     { path: 'slot', select: '-createdAt -updatedAt -__v' },
+  //   ]);
+
+  //   // end session
+  //   await session.commitTransaction();
+  //   await session.endSession();
+
+  //   // return result with populated customer, service and slot details
+  //   return result;
+  // } catch (err: any) {
+  //   // rollback session in case of error
+  //   await session.abortTransaction();
+  //   await session.endSession();
+  //   throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, err.message);
+  // }
 };
 
 const getAllBookings = async () => {
